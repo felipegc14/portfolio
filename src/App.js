fef550bf6044dca3922467be2b01350add6f69f1
felipe gomez castro <felipegc000@gmail.com>
2022-04-19 20:13:23 -0300

    primer cambio

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='container'>
+        <h1>hola mundo</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
